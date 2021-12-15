67bc97798ab42ed761598370f1dbd10425374e0b
Yuening Xu <xuyn@gwu.edu>
2021-12-15 03:05:20 -0500

    seperation and cohesion(partly)

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -32,9 +32,11 @@ rotation_opt; // 1 for fixed angle, 2 for quaternion
 
 //allocate memory for 10 boids
 int boidnum = 10;
-float boid[10][15];//boid matrix
-float bv[10][2];//boid velocity on x and y
-float bf[10][2];//force on boid
+float boid[10][15] = {0};//boid matrix
+float bv[10][2] = {0};//boid velocity on x and y
+float bf[10][2] = {0};//force on boid
+float rs = 3; // radius of seperation detection. 
+float rc = 11; // radius of cohesion
 
 float point_num = 6;
 //geometric point for quaternion
@@ -79,17 +81,14 @@ void drawBoids(int bn) {
 	//calculate velocity
 	bv[bn][0] = bv[bn][0] + ax * 0.01;
 	bv[bn][1] = bv[bn][1] + ay * 0.01;
-	//add gravity
 
-	/***
-	float x = boid[bn][12] + ballv[bn][0] * 0.01;
-	float y = boid[bn][13] + ballv[bn][1] * 0.01;
+	//update location
+	float x = boid[bn][12] + bv[bn][0] * 0.01;
+	float y = boid[bn][13] + bv[bn][1] * 0.01;
 
-	ballloc[bn][0] = x;
-	ballloc[bn][1] = y;
-	***/
-	boid[bn][12] += bv[bn][0] * 0.01;
-	boid[bn][13] += bv[bn][1] * 0.01;
+	boid[bn][12] = x;
+	boid[bn][13] = y;
+
 
 	glPushMatrix();
 	M[0] = 1.0f;
@@ -105,68 +104,96 @@ void drawBoids(int bn) {
 	glPopMatrix();
 }
 
-//Matrix multiplication for Q(t) = TMG
-float matrixTMG(float M[4][4], float G[4]) {
-	float TMG;//return value
-	float MG[4];//valur of M * G
-	float mt = t - int(t);
-	MG[0] = mCR[0][0] * G[0] + mCR[0][1] * G[1] + mCR[0][2] * G[2] + mCR[0][3] * G[3];
-	MG[1] = mCR[1][0] * G[0] + mCR[1][1] * G[1] + mCR[1][2] * G[2] + mCR[1][3] * G[3];
-	MG[2] = mCR[2][0] * G[0] + mCR[2][1] * G[1] + mCR[2][2] * G[2] + mCR[2][3] * G[3];
-	MG[3] = mCR[3][0] * G[0] + mCR[3][1] * G[1] + mCR[3][2] * G[2] + mCR[3][3] * G[3];
-	TMG = MG[0] * mt * mt * mt + MG[1] * mt * mt + MG[2] * mt + MG[3];
-	return TMG;
+void separate(int bn) {
+	// if a boid is 2 unit close to another, a force will compress it to go away
+	for (int i = 0; i < boidnum; i++) {
+		if (i != bn) {
+			float distx = boid[bn][12] - boid[i][12];
+			float disty = boid[bn][13] - boid[i][13];
+			float distxy = sqrtf(distx * distx + disty * disty);
+			if (distxy < rs) {
+				//add force on x axis
+				if (boid[bn][12] > boid[i][12]) {
+					bf[bn][0] =0;
+					//change velocity according to distxy
+					bv[bn][0] += 0.5 ;
+				}
+				else {
+					bf[bn][0] = 0;
+					//bf[bn][0] -= 0.5;
+					bv[bn][0] -= 0.5;
+				}
+				//add force on y axis
+				if (boid[bn][13] > boid[i][13]) {
+					bf[bn][1] = 0;
+					//bf[bn][1] += 0.5;
+					bv[bn][1] += 0.5;
+
+				}
+				else {
+					bf[bn][1] = 0;
+					//bf[bn][1] -= 0.5;
+					bv[bn][1] -= 0.5;
+				}
+			}
+		}
+
+	}
 }
 
-//================================
-// Quaternion rotation 
-// ===============================
-void quaternion(float w, float qx, float qy, float qz) {
-	M[0] = 1.0f - 2.0f * (qy * qy + qz * qz);
-	M[1] = 2.0f * (qx * qy + w * qz);
-	M[2] = 2.0f * (qx * qz - w * qy);
-	M[3] = 0.0f;
-	M[4] = 2.0f * (qx * qy - w * qz);
-	M[5] = 1.0f - 2.0f * (qx * qx + qz * qz);
-	M[6] = 2.0f * (qy * qz + w * qx);
-	M[7] = 0.0f;
-	M[8] = 2.0f * (qx * qz + w * qy);
-	M[9] = 2.0f * (qy * qz - w * qx);
-	M[10] = 1 - 2.0f * (qx * qx + qy * qy);
-	M[11] = 0.0f;
-	M[12] = x;
-	M[13] = y;
-	M[14] = z;
-	M[15] = 1.0f;
-}
+void cohesion(int bn) {
+	//a boid show move toward the average postion of local boids.
+	//local boids: boids in radius cohesion 
+	float targetxt = boid[bn][12];//target x total
+	float targetyt = boid[bn][13];//target y total
+	int num = 0;//boids num in cohesion radius
 
-//================================
-//fixed angle rotation
-//================================
-void fixedangle(float fx, float fy, float fz) {
-	//use pi to represent degree
-	double dx = fx * PI / 180.0;
-	double dy = fy * PI / 180.0;
-	double dz = fz * PI / 180.0;
-	//assume rotate about x first, then y, then z
-	M[0] = cos(dz) * cos(dy);
-	M[1] = sin(dz) * cos(dy);
-	M[2] = -sin(dy);
-	M[3] = 0.0f;
-	M[4] = cos(dz) * sin(dy) * sin(dx) - sin(dz) * cos(dx);
-	M[5] = sin(dz) * sin(dy) * sin(dx) + cos(dz) * cos(dx);
-	M[6] = cos(dy) * sin(dx);
-	M[7] = 0.0f;
-	M[8] = cos(dz) * sin(dy) * cos(dx) + sin(dz) * sin(dx);
-	M[9] = sin(dz) * sin(dy) * cos(dx) - cos(dz) * sin(dx);
-	M[10] = cos(dy) * cos(dx);
-	M[11] = 0.0f;
-	M[12] = x;
-	M[13] = y;
-	M[14] = z;
-	M[15] = 1.0f;
-}
+	for (int i = 0; i < boidnum; i++) {
+		float distx = boid[bn][12] - boid[i][12];
+		float disty = boid[bn][13] - boid[i][13];
+		float distxy = sqrtf(distx * distx + disty * disty);
+		if (distxy < rc) {
+			//std::cout << i;
+			targetxt += boid[i][12]; //if the boid is in the range, add it's location to the avrage x;
+			targetyt += boid[i][13]; //.. y...
+			num++;
+		}
+	}
 
+	float targetx = targetxt / num;
+	float targety = targetyt / num;
+
+	if (boid[bn][12] > targetx) {
+		//find velocity target
+		float vx = (boid[bn][12] - targetx)/10;
+		float ax = abs(vx - bv[bn][0]) / 10;
+		if (abs(bf[bn][0]) < 10) {
+			bf[bn][0] -= ax;
+		}
+
+	}
+	else {
+		float vx = (boid[bn][12] - targetx) / 10;
+		float ax = abs(vx - bv[bn][0]) / 10;
+		if (abs(bf[bn][0]) < 10) {
+			bf[bn][0] += ax;
+		}
+	}
+	if (boid[bn][13] > targety) {
+		float vy = (boid[bn][13] - targety) / 10;
+		float ay = abs(vy - bv[bn][1]) / 10;
+		if (abs(bf[bn][1]) < 10) {
+			bf[bn][1] -= ay;
+		}
+	}
+	else {
+		float vy = (boid[bn][13] - targety) / 10;
+		float ay = abs(vy - bv[bn][1]) / 10;
+		if (abs(bf[bn][1]) < 10) {
+			bf[bn][1] += ay;
+		}
+	}
+}
 
 //================================
 // init
@@ -177,11 +204,12 @@ void init(void) {
 	for (int i = 0; i < boidnum; i++) {
 		boid[i][12] = rand() % 19 + (-9); //limit by windows width x in range(-9, 9)
 		boid[i][13] = rand() % 15 + (-7);//limit by windows height y in range(-7, 7);
-		boid[i][14] = -25;//make it 2d
-		bv[i][0] = rand() % 11 + (-5); //limit speed on x in range (-5, 5)
-		bv[i][1] = rand() % 11 + (-5); //limit speed on y in range (-5, 5)
+		boid[i][14] = -30;//make it 2d
+		bv[i][0] = 0;
+			//rand() % 11 + (-5); //limit speed on x in range (-5, 5)
+		bv[i][1] = 0;
+			//rand() % 11 + (-5); //limit speed on y in range (-5, 5)
 		//std::cout << i;
-		//printf("i = %d x = %f y = %f z = %f\n", i, boid[i][12], boid[i][13], boid[i][14]);
 	}
 	
 }
@@ -339,7 +367,9 @@ void render(void) {
 	glLoadIdentity();
 	//glTranslatef(0.0, 0.0, -20);
 	//displayTeapot();
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < boidnum; i++) {
+		separate(i);
+		cohesion(i);
 		drawBoids(i);
 	}
 	// disable lighting
